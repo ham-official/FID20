d57c0b183db28792af3ea4c5fd388b1f8eb1fb82
apexethdev <apexethdev@gmail.com>
2024-06-17 23:10:40 +0100

    update to token contract

--- src/ExampleFRCToken.sol
+++ src/ExampleFRCToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.23;
 
 import {FRC20} from "src/FRC20/FRC20.sol";
 
-contract ExampleFRCToken is FRC20, {
+contract ExampleFRCToken is FRC20 {
 
     /// FID Contract Address on HAM L3
     address public fidStorageAddress = 0xCca2e3e860079998622868843c9A00dEbb591D30;
